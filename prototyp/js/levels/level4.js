7d9c71a3cea34bd5911c0e6f6c0026de03d9fe8c
silvan1999 <silvan.rossau@hftm.ch>
2025-08-14 10:49:15 +0200

    Update level4.js
    die angepasste Version

--- prototyp/js/levels/level4.js
+++ prototyp/js/levels/level4.js
@@ -47,67 +47,98 @@ Standardmässig isch das 0 drum wenn mes nid schribt, denn gits ke delay.
 */
 
 function initLevel() {
-    // Spieler-Startposition - cha me apasse
-    player.x = 100;
-    player.y = world.height - 130;
-    player.spawnPoint = { x: player.x, y: player.y };
+	// Spieler-Startposition
+	player.x = 100;
+	player.y = world.height - 130;
+	player.spawnPoint = { x: player.x, y: player.y };
 
-
-    levelObjects = [
-        // Boden über ganzes level
-        { x: -1000, y: world.height - 100, width: world.width + 1000, height: 100, type: 'platform' },
+	levelObjects = [
+		// Boden über ganzes level
+		{ x: -1000, y: world.height - 100, width: world.width + 1000, height: 100, type: 'platform' },
 		// Wand level ende
 		{ x: -1000, y: world.height - 400, width: 50, height: 500, type: 'platform' },
-				
-        // Duck-Passage: zum Ducken mit S
-        { x: 300, y: world.height - 275, width: 200, height: 150, type: 'platform' },
 
-        // Gegner hinter Duck-Passage
-        { x: 600, y: world.height - 130, width: 30, height: 30, type: 'danger' },
+		// Zungenobjekt über Plattform
+		{ x: 150, y: world.height - 450, width: 20, height: 20, type: 'tongue' },
 
-        // Plattform zum Springen 
-        { x: 900, y: world.height - 300, width: 150, height: 20, type: 'platform' },
+		// Duck-Passage
+		{ x: 170, y: world.height - 275, width: 100, height: 180, type: 'platform' },
+		{ x: 300, y: world.height - 400, width: 20, height: 180, type: 'platform' },
+		{ x: 320, y: world.height - 150, width: 20, height: 200, type: 'platform' },
+		{ x: 380, y: world.height - 150, width: 30, height: 30, type: 'danger' },
+		{ x: 420, y: world.height - 180, width: 20, height: 300, type: 'platform' },
+		{ x: 470, y: world.height - 275, width: 100, height: 180, type: 'platform' },
+		{ x: 500, y: world.height - 400, width: 20, height: 50, type: 'platform' },
+		{ x: 580, y: world.height - 450, width: 20, height: 120, type: 'platform' },
 
-        // Zungenobjekt über Plattform
-        { x: 1150, y: world.height - 450, width: 20, height: 20, type: 'tongue' },
+		{ x: 620, y: world.height - 150, width: 20, height: 200, type: 'platform' },
+		{ x: 640, y: world.height - 130, width: 30, height: 30, type: 'catcher', moving: true, axis: 'x', min: 800, max: 950, speed: 4, direction: 1 },
+		{ x: 650, y: world.height - 230, width: 30, height: 30, type: 'danger' },
+		{ x: 720, y: world.height - 180, width: 20, height: 300, type: 'platform' },
+		{ x: 800, y: world.height - 280, width: 20, height: 100, type: 'platform' },
+		{ x: 900, y: world.height - 300, width: 150, height: 20, type: 'platform' },
 
-        // Catcher danach (nur mit Tarnung durch) -- mit bewegung
-		{
-			x: 1600,
-			y: world.height - 130,
-			width: 30,
-			height: 30,
-			type: 'catcher',
-			moving: true, //wenn false, denn cha mes eifach wegloh und es isch okay. wenn sichs bewege söu denn muess true si
-			axis: 'x', // x oder y
-			min: 1500, //endposition links bzw oben wenn y achse
-			max: 1700, //endposition rechts bzw unte
-			speed: 1, // desto grösser desto schneller (pixel + oder - pro zyklus)
-			direction: 1 // 1 heisst me startet nach rechts wenn x achse/ nach unte wenn y achse. mit 0 geit me nach links / ufe. Nid würk relevant (wird immer bim min und max gswitched)		
-		},
+		{ x: 1000, y: world.height - 210, width: 30, height: 30, type: 'catcher', moving: true, axis: 'x', min: 1000, max: 1200, speed: 4, direction: 1 },
+		{ x: 1000, y: world.height - 180, width: 250, height: 100, type: 'platform' },
+		{ x: 1150, y: world.height - 550, width: 20, height: 20, type: 'tongue' },
 
-        // Plattform moving 
-        {
-			x: 1350,
-			y: world.height - 250,
-			width: 100,
-			height: 20,
-			type: 'platform',
-			moving: true,
-			axis: 'y',
-			min: world.height - 500, 
-			max: world.height -101, 
-			speed: 1,
-			direction: 1,
-			pauseDuration: 60 // das si ke milli-sekunde oder so. Das si frames. 60fps (frames pro sekunde) si standard. Drum 60 = 1s (öppe)
-		},
+		{ x: 1300, y: world.height - 275, width: 300, height: 120, type: 'platform' },
+		{ x: 1300, y: world.height - 305, width: 30, height: 30, type: 'catcher', moving: true, axis: 'x', min: 1300, max: 1600, speed: 5, direction: 1 },
+		{ x: 1350, y: world.height - 250, width: 100, height: 20, type: 'platform', moving: true, axis: 'y', min: world.height - 600, max: world.height - 401, speed: 1, direction: 1, pauseDuration: 60 },
 
+		{ x: 1800, y: world.height - 130, width: 30, height: 30, type: 'catcher', moving: true, axis: 'x', min: 2000, max: 2600, speed: 1, direction: 1 },
+		{ x: 1800, y: world.height - 305, width: 30, height: 30, type: 'catcher', moving: true, axis: 'x', min: 2000, max: 2600, speed: 3, direction: 1 },
+		{ x: 1800, y: world.height - 130, width: 30, height: 30, type: 'catcher', moving: true, axis: 'x', min: 2700, max: 3100, speed: 3, direction: 1 },
+		{ x: 1800, y: world.height - 200, width: 40, height: 40, type: 'platform' },
+		{ x: 1800, y: world.height - 190, width: 40, height: 40, type: 'platform' },
+		{ x: 1800, y: world.height - 110, width: 30, height: 10, type: 'checkpoint' },
 
-        // Checkpoint
-        { x: 1800, y: world.height - 110, width: 30, height: 10, type: 'checkpoint' },
+		{ x: 2000, y: world.height - 220, width: 40, height: 40, type: 'platform' },
 
-        // Ziel ganz rechts
-        { x: 2000, y: world.height - 130, width: 30, height: 30, type: 'goal' }
-    ];
+		{ x: 2200, y: world.height - 275, width: 60, height: 80, type: 'platform' },
+
+		{ x: 2350, y: world.height - 250, width: 100, height: 20, type: 'platform', moving: true, axis: 'y', min: world.height - 600, max: world.height - 401, speed: 1, direction: 1, pauseDuration: 60 },
+
+		{ x: 2400, y: world.height - 220, width: 40, height: 40, type: 'platform' },
+		{ x: 2450, y: world.height - 600, width: 20, height: 20, type: 'tongue' },
+
+		{ x: 2600, y: world.height - 250, width: 40, height: 40, type: 'platform' },
+
+		{ x: 2800, y: world.height - 600, width: 20, height: 20, type: 'tongue' },
+		{ x: 2800, y: world.height - 275, width: 60, height: 80, type: 'platform' },
+
+		{ x: 3000, y: world.height - 130, width: 30, height: 30, type: 'catcher', moving: true, axis: 'x', min: 1500, max: 1700, speed: 1, direction: 1 },
+
+		{ x: 3150, y: world.height - 600, width: 20, height: 20, type: 'tongue' },
+
+		{ x: 3200, y: world.height - 130, width: 30, height: 30, type: 'danger' },
+
+		{ x: 3300, y: world.height - 230, width: 30, height: 250, type: 'platform' },
+		
+		{ x: 3800, y: world.height - 230, width: 30, height: 250, type: 'platform' },
+
+		{ x: 3350, y: world.height - 250, width: 100, height: 20, type: 'platform', moving: true, axis: 'y', min: world.height - 800, max: world.height - 401, speed: 1, direction: 1, pauseDuration: 60 },
+
+		{ x: 4100, y: world.height - 130, width: 30, height: 30, type: 'danger' },
+
+		{ x: 4150, y: world.height - 600, width: 20, height: 20, type: 'tongue' },
+
+		{ x: 4200, y: world.height - 130, width: 30, height: 30, type: 'catcher', moving: true, axis: 'x', min: 1500, max: 1700, speed: 1, direction: 1 },
+
+		{ x: 4300, y: world.height - 230, width: 400, height: 10, type: 'platform' },
+
+		{ x: 4350, y: world.height - 250, width: 100, height: 20, type: 'platform', moving: true, axis: 'y', min: world.height - 800, max: world.height - 401, speed: 1, direction: 1, pauseDuration: 60 },
+
+		{ x: 4600, y: world.height - 130, width: 30, height: 30, type: 'catcher', moving: true, axis: 'x', min: 1500, max: 1700, speed: 1, direction: 1 },
+
+		{ x: 4800, y: world.height - 110, width: 30, height: 10, type: 'checkpoint' },
+
+		{ x: 5200, y: world.height - 450, width: 20, height: 20, type: 'tongue' },
+
+		{ x: 5300, y: world.height - 275, width: 300, height: 200, type: 'platform' },
+
+		{ x: 5900, y: world.height - 130, width: 30, height: 30, type: 'danger' },
+
+		{ x: 6000, y: world.height - 130, width: 30, height: 30, type: 'goal' }
+	];
 }
-
