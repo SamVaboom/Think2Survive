ac8c2727eed1ffa73d0e51d2b1d62ed1df083a8d
silvan1999 <silvan.rossau@hftm.ch>
2025-08-21 14:55:20 +0200

    Update level4.js
    Updated Version

--- prototyp/js/levels/level4.js
+++ prototyp/js/levels/level4.js
@@ -1,51 +1,3 @@
-/*
-ERKLÄRUNG KOORDINATENSYSTEM:
-Level isch 6000 * 2500 px gross. Es isch möglich Objekt usserhalb vo däm z'platziere.
-
-y
-|a*									 						b* 
-|
-|
-|
-|
-|
-|
-|
-|c* 														d* 
-|_______________________________________________________________x
-
-a* -> das isch (0, 0) ganz obe links
-b* -> das isch (6000, 0) ganz obe rechts
-c* -> das isch (0, 2500) ganz unte rechts
-d* -> das isch (6000, 2500) ganz unte rechts 
-
-Das heisst desto grösser x desto weiter rechts - desto grösser y desto witer unte.
-
-world.height = 2500
-world.width = 6000
-world.height / 2 und world.width / 2 isch mitti vom level
-
-Wenn me y: world.height - "zahl" macht denn ischs chli eifacher zum sich vorstelle wius denn wie es normals koordinatesystem isch ("zahl" grösser heisst witter obe und "world.height -" cha me sich wegdenke)
-
-Player cha 250 wit und 
-*/
-
-
-/*
-ERKLÄRUNG BEWEGUNG / MOVING:
-
-Es isch relativ simpel, eifach rechts nach links und ue nach abe und zrugg - Man cha jedem levelObjekt folgendi attribut zueweise:
-moving, axis, min, max, speed und direction. (Ig ha alli die attribut unte im ne biispiu erklärt)
-
-Me cha das allem zuewiise!! Egal welem Level-Objekt (plattform, danger, goal usw.)
-
-Denn hani no e delay hinzuegfüegt. Das isch optional aber ig ha dänkt es isch vllt praktisch für de time-basierti hindernis. 
-De delay macht dass s'Objekt bim min und max stah blibt für x time. Me muess folgendes attribut hinzuefüege:
-pauseDuration (biispiu unte)
-
-Standardmässig isch das 0 drum wenn mes nid schribt, denn gits ke delay. 
-*/
-
 function initLevel() {
 	// Spieler-Startposition
 	player.x = 100;
@@ -62,6 +14,9 @@ function initLevel() {
 		{ x: 150, y: world.height - 450, width: 20, height: 20, type: 'tongue' },
 
 		// Duck-Passage
+	
+		
+		
 		{ x: 170, y: world.height - 275, width: 100, height: 180, type: 'platform' },
 		{ x: 300, y: world.height - 400, width: 20, height: 180, type: 'platform' },
 		{ x: 320, y: world.height - 150, width: 20, height: 200, type: 'platform' },
@@ -118,27 +73,42 @@ function initLevel() {
 		{ x: 3800, y: world.height - 230, width: 30, height: 250, type: 'platform' },
 
 		{ x: 3350, y: world.height - 250, width: 100, height: 20, type: 'platform', moving: true, axis: 'y', min: world.height - 800, max: world.height - 401, speed: 1, direction: 1, pauseDuration: 60 },
+	
+		{ x: 3300, y: world.height - 130, width: 30, height: 30, type: 'catcher', moving: true, axis: 'x', min: 3400, max: 3650, speed: 1, direction: 1 },
+	{ x: 3700, y: world.height - 230, width: 200, height: 60, type: 'platform' },
+	{ x: 3900, y: world.height - 330, width: 200, height: 60, type: 'platform' },	
 
-		{ x: 4100, y: world.height - 130, width: 30, height: 30, type: 'danger' },
 
 		{ x: 4150, y: world.height - 600, width: 20, height: 20, type: 'tongue' },
 
-		{ x: 4200, y: world.height - 130, width: 30, height: 30, type: 'catcher', moving: true, axis: 'x', min: 1500, max: 1700, speed: 1, direction: 1 },
-
+		{ x: 4300, y: world.height - 270, width: 30, height: 30, type: 'catcher', moving: true, axis: 'x', min: 4300, max: 4700, speed: 1, direction: 1 },
 		{ x: 4300, y: world.height - 230, width: 400, height: 10, type: 'platform' },
 
-		{ x: 4350, y: world.height - 250, width: 100, height: 20, type: 'platform', moving: true, axis: 'y', min: world.height - 800, max: world.height - 401, speed: 1, direction: 1, pauseDuration: 60 },
+		{ x: 4300, y: world.height - 230, width: 400, height: 10, type: 'platform' },
+		{ x: 4500, y: world.height - 530, width: 200, height: 10, type: 'platform' },
 
-		{ x: 4600, y: world.height - 130, width: 30, height: 30, type: 'catcher', moving: true, axis: 'x', min: 1500, max: 1700, speed: 1, direction: 1 },
-
+		
+		{ x: 4300, y: world.height - 440, width: 30, height: 30, type: 'catcher', moving: true, axis: 'x', min: 4300, max: 4600, speed: 1, direction: 1 },
+		{ x: 4800, y: world.height - 440, width: 30, height: 30, type: 'catcher', moving: true, axis: 'x', min: 4800, max: 6000, speed: 1, direction: 1 },
+		{ x: 4400, y: world.height - 400, width: 400, height: 60, type: 'platform' },
+		
+{ x: 4300, y: world.height - 130, width: 30, height: 30, type: 'catcher', moving: true, axis: 'x', min: 4000, max: 4700, speed: 5, direction: 1 },
+{ x: 4800, y: world.height - 130, width: 30, height: 30, type: 'catcher', moving: true, axis: 'x', min: 4900, max: 6000, speed: 5, direction: 1 },
 		{ x: 4800, y: world.height - 110, width: 30, height: 10, type: 'checkpoint' },
 
-		{ x: 5200, y: world.height - 450, width: 20, height: 20, type: 'tongue' },
-
-		{ x: 5300, y: world.height - 275, width: 300, height: 200, type: 'platform' },
-
-		{ x: 5900, y: world.height - 130, width: 30, height: 30, type: 'danger' },
-
-		{ x: 6000, y: world.height - 130, width: 30, height: 30, type: 'goal' }
+		{ x: 4800, y: world.height - 250, width: 30, height: 20, type: 'platform' },
+		{ x: 4900, y: world.height - 280, width: 30, height: 20, type: 'platform' },
+		{ x: 5000, y: world.height - 400, width: 30, height: 20, type: 'platform' },
+		{ x: 5100, y: world.height - 520, width: 30, height: 20, type: 'platform' },
+		{ x: 5200, y: world.height - 250, width: 30, height: 20, type: 'platform' },
+		{ x: 5300, y: world.height - 320, width: 30, height: 20, type: 'platform' },
+		{ x: 5400, y: world.height - 420, width: 30, height: 20, type: 'platform' },
+		{ x: 5500, y: world.height - 510, width: 30, height: 20, type: 'platform' },		
+		{ x: 5600, y: world.height - 500, width: 30, height: 20, type: 'platform' },
+		{ x: 5700, y: world.height - 580, width: 30, height: 20, type: 'platform' },
+		{ x: 5800, y: world.height - 600, width: 30, height: 20, type: 'platform' },
+		{ x: 6000, y: world.height - 650, width: 30, height: 30, type: 'goal' }
 	];
 }
+
+
